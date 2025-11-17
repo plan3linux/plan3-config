74f35675126b5565c937e233e47d74efee71bd9b
InfiTechPPC <tifasoftware@gmx.com>
2025-11-17 15:07:48 -0600

    Start making installer

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+paru -S bspwm emacs i3-gaps i3blocks i3lock i3status kvantum openbox polybar qtile sxhkd volumeicon dolphin zen-browser-bin
+cp -r .config ~
+cp -r .local ~
+cp -r .themes ~
+cp -r .icons ~
