7bd27f68b81dda93c67e660d12eee8ee9cf1b8b7
InfiTechPPC <tifasoftware@gmx.com>
2025-11-18 18:19:48 -0600

    added zsh plugins

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 echo "Installing Packages"
 paru -Syyu bspwm zsh emacs i3-wm i3blocks i3lock i3status kvantum openbox polybar qtile sxhkd volumeicon thunar zen-browser-bin waypaper feh picom
-paru -S thunar-volman thunar-archive-plugin thunar-media-tags-plugin gvfs catfish tumbler
-paru -S lxappearance
+paru -S thunar-volman thunar-archive-plugin thunar-media-tags-plugin gvfs catfish tumbler xclip
+paru -S lxappearance starship zoxide
 
 echo "Copying Configuration Files"
 cp -r .config ~
@@ -18,7 +18,10 @@ echo "Compiling Suckless Suite (DWM, ST, DRUN)"
 
 echo "Installing Doom Emacs"
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
-~/.config/emacs/bin/doom install
+#~/.config/emacs/bin/doom install
+~/.config/emacs/bin/doom sync
+~/.config/emacs/bin/doom env
+emacs --batch -f nerd-fonts-install-fonts
 
 echo "Installing LazyVim"
 git clone https://github.com/LazyVim/starter ~/.config/nvim
@@ -27,6 +30,7 @@ echo "Install ZSH Goodies"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/z-shell/F-Sy-H.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/F-Sy-H
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+starship preset gruvbox-rainbow -o ~/.config/starship.toml
 
 echo "Overwriting ZSH Config"
 cp p3-zsh ~/.zshrc
